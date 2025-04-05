81cf7f53256d8fa2eb7a87dcc59b5ab955a38831
Niloy Irtisam <niloyirtisam@hotmail.com>
2025-04-05 22:14:29 +0100

    Added ViT

--- src/models/tvmodels.py
+++ src/models/tvmodels.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torchvision.models as tvmodels
 
 
-__all__ = ["mobilenet_v3_small", "vgg16"]
+__all__ = ["mobilenet_v3_small", "vgg16", "vit_b_16"]
 
 
 class TorchVisionModel(nn.Module):
@@ -57,6 +57,15 @@ def mobilenet_v3_small(num_classes, loss={"xent"}, pretrained=True, **kwargs):
     )
     return model
 
+def vit_b_16(num_classes, loss={"xent"}, pretrained=True, **kwargs):
+    model = TorchVisionModel(
+        "vit_b_16",
+        num_classes=num_classes,
+        loss=loss,
+        pretrained=pretrained,
+        **kwargs,
+    )
+    return model
 
 # Define any models supported by torchvision bellow
 # https://pytorch.org/vision/0.11/models.html
