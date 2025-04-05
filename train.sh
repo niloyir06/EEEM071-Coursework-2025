2d3329cd89e3afa07d5edae135c2448f19d7b03f
Niloy Irtisam <niloyirtisam@hotmail.com>
2025-04-05 18:36:11 +0100

    Changes

--- train.sh
+++ train.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+eval "$(conda shell.bash hook)"
+conda activate pytorch
+
+MODEL=mobilenet_v3_small
+OPTIMIZER=amsgrad
+LR=0.0003
+SAVE_DIR="$HOME/EEEM071/coursework/logs/${MODEL}_${OPTIMIZER}_${LR}"
+
 STUDENT_ID=kn00794 STUDENT_NAME="Jane Doe" python main.py \
 -s veri \
 -t veri \
--a mobilenet_v3_small \
---root /content \
+-a $MODEL \
+--root ../ \
 --height 224 \
 --width 224 \
---optim amsgrad \
---lr 0.0003 \
+--optim $OPTIMIZER \
+--lr $LR \
 --max-epoch 10 \
 --stepsize 20 40 \
 --train-batch-size 64 \
 --test-batch-size 100 \
---save-dir logs/mobilenet_v3_small-veri
+--save-dir $SAVE_DIR
